d9a474dd6026f7ee915c8827f75c1fe1f03cd900
Daniel Wirz <d.wirz@iqual.ch>
2022-12-06 12:14:26 +0100

    add scrolling to activate event

--- patterns/iq-faq-item/faq-item.js
+++ patterns/iq-faq-item/faq-item.js
@@ -1,7 +1,17 @@
 (function ($, Drupal) {
   setListenerForScrollOffsetCalculation('scroll, resize');
   function generateAccordion($stacks) {
-    var definition = { icons: false, heightStyle: 'content', collapsible: true, active: false };
+    var definition = {
+      icons: false,
+      heightStyle: 'content',
+      collapsible: true,
+      active: false,
+      activate: function( event, ui ) {
+        if(!$.isEmptyObject(ui.newHeader.offset())) {
+          $('html:not(:animated), body:not(:animated)').animate({ scrollTop: ui.newHeader.offset().top - window.scrollOffset }, 'slow');
+        }
+      }
+    };
     var $first = null;
     var newGroup = true;
     var type = '';
